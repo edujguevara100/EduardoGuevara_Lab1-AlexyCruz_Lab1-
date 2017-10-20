3faa24c03a6282703a2c4957f1f60c6998c25c09
edujguevara100 <edujguevara100@gmail.com>
2017-10-20 13:51:14 -0600

    Clase maestro ya tiene los set

---
+++ EduardoGuevara_Lab1(AlexyCruz_Lab1)/src/eduardoguevara_lab1/alexycruz_lab1/EduardoGuevara_Lab1AlexyCruz_Lab1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eduardoguevara_lab1.alexycruz_lab1;
+
+/**
+ *
+ * @author edujg
+ */
+public class EduardoGuevara_Lab1AlexyCruz_Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
